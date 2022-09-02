c072419705a5f2b64d5463b239eb169109d24b7f
baovietctg <baovietctg@gmail.com>
2022-09-02 15:25:27 +0700

    fix code

--- baitapRest/js/main2.js
+++ baitapRest/js/main2.js
@@ -1,1 +1,31 @@
 //Bài 2 - tính điểm trung bình
+let tinhDiemTB = (...scores) => {
+    let tongDiem = 0;
+    let diemTB = 0;
+    let count = 0;
+    scores.map((number) => { 
+        tongDiem += number;
+        count++;
+        diemTB = tongDiem/count;
+     })
+     return diemTB.toFixed(2);
+}
+
+let tinhDiemTB1 = () => {
+    let toan = Number(document.getElementById("inpToan").value);
+    let ly = Number(document.getElementById("inpLy").value);
+    let hoa = Number(document.getElementById("inpHoa").value);
+    let diemTB = tinhDiemTB(toan, ly, hoa);
+    document.getElementById("tbKhoi1").innerHTML = diemTB;
+}
+document.getElementById("btnKhoi1").onclick = tinhDiemTB1;
+
+let tinhDiemTB2 = () => {
+    let van = Number(document.getElementById("inpVan").value);
+    let su = Number(document.getElementById("inpSu").value);
+    let dia = Number(document.getElementById("inpDia").value);
+    let english = Number(document.getElementById("inpEnglish").value);
+    let diemTB = tinhDiemTB(van, su, dia, english);
+    document.getElementById("tbKhoi2").innerHTML = diemTB;
+}
+document.getElementById("btnKhoi2").onclick = tinhDiemTB2;
---
+++ baitapSpread/js/main3.js
@@ -0,0 +1,14 @@
+//Bài 3: Hover text
+let hover = () => {
+    let str = "Hover Me!"
+    let content = "";
+    let str2 = [...str];
+    str2.map((zoom) => { 
+        content +=`
+            <span>${zoom}</span>
+        `
+    });
+   document.querySelector(".heading").innerHTML = content;
+
+}
+hover();
